e7d6237cba8e25d91681706ea9ccd6a6c7b49139
Jesse Andrews <anotherjesse@gmail.com>
2021-10-10 14:56:10 -0400

    change entire background to the color sent via mqtt

--- code.py
+++ code.py
@@ -2,14 +2,12 @@
 # SPDX-License-Identifier: MIT
 
 import board
-import busio
 import time
-from digitalio import DigitalInOut
-import adafruit_requests as requests
-import adafruit_esp32spi.adafruit_esp32spi_socket as socket
-from adafruit_esp32spi import adafruit_esp32spi
+import terminalio
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_matrixportal.matrixportal import MatrixPortal
+from adafruit_matrixportal.network import Network
 
 
 # Get wifi details and more from a secrets.py file
@@ -19,59 +17,22 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-print("ESP32 SPI webclient test")
+# print("ESP32 SPI webclient test")
 
-TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
+# TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
 
+matrixportal = MatrixPortal(status_neopixel=board.NEOPIXEL, debug=True)
+network = matrixportal.network
+network.connect()
 
-# If you are using a board with pre-defined ESP32 Pins:
-esp32_cs = DigitalInOut(board.ESP_CS)
-esp32_ready = DigitalInOut(board.ESP_BUSY)
-esp32_reset = DigitalInOut(board.ESP_RESET)
-
-# If you have an AirLift Shield:
-# esp32_cs = DigitalInOut(board.D10)
-# esp32_ready = DigitalInOut(board.D7)
-# esp32_reset = DigitalInOut(board.D5)
-
-# If you have an AirLift Featherwing or ItsyBitsy Airlift:
-# esp32_cs = DigitalInOut(board.D13)
-# esp32_ready = DigitalInOut(board.D11)
-# esp32_reset = DigitalInOut(board.D12)
-
-# If you have an externally connected ESP32:
-# NOTE: You may need to change the pins to reflect your wiring
-# esp32_cs = DigitalInOut(board.D9)
-# esp32_ready = DigitalInOut(board.D10)
-# esp32_reset = DigitalInOut(board.D5)
-
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
-
-requests.set_socket(socket, esp)
-
-if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
-    print("ESP32 found and in idle mode")
-print("Firmware vers.", esp.firmware_version)
-print("MAC addr:", [hex(i) for i in esp.MAC_address])
-
-for ap in esp.scan_networks():
-    print("\t%s\t\tRSSI: %d" % (str(ap["ssid"], "utf-8"), ap["rssi"]))
-
-print("Connecting to AP...")
-while not esp.is_connected:
-    try:
-        esp.connect_AP(secrets["ssid"], secrets["password"])
-    except RuntimeError as e:
-        print("could not connect to AP, retrying: ", e)
-        continue
-print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
-print("My IP address is", esp.pretty_ip(esp.ip_address))
-print(
-    "IP lookup adafruit.com: %s" % esp.pretty_ip(esp.get_host_by_name("adafruit.com"))
+matrixportal.add_text(
+    text_font=terminalio.FONT,
+    text_position=(0, (matrixportal.graphics.display.height // 2) - 1),
+    scrolling=False,
 )
 
-mqtt_topic = "test/topic"
+# matrixportal.set_text("hiya world")
+
 
 def connect(mqtt_client, userdata, flags, rc):
     # This function will be called when the mqtt_client is connected
@@ -95,16 +56,14 @@ def unsubscribe(mqtt_client, userdata, topic, pid):
     print("Unsubscribed from {0} with PID {1}".format(topic, pid))
 
 
-def publish(mqtt_client, userdata, topic, pid):
-    # This method is called when the mqtt_client publishes data to a feed.
-    print("Published to {0} with PID {1}".format(topic, pid))
-
-
 def message(client, topic, message):
+    matrixportal.set_background(int(message, 16))
+    # matrixportal.set_text_color('#'+message)
+    # matrixportal.set_text(message)
     # Method callled when a client's subscribed feed has a new value.
     print("New message on topic {0}: {1}".format(topic, message))
 
-mqtt_client = MQTT.MQTT(
+m = MQTT.MQTT(
     broker=secrets["broker"],
     port=secrets["port"],
     socket_pool=socket,
@@ -112,33 +71,22 @@ mqtt_client = MQTT.MQTT(
 )
 
 mqtt_topic = "matrix"
-mqtt_client.on_connect = connect
-mqtt_client.on_disconnect = disconnect
-mqtt_client.on_subscribe = subscribe
-mqtt_client.on_unsubscribe = unsubscribe
-mqtt_client.on_publish = publish
-mqtt_client.on_message = message
+m.on_connect = connect
+m.on_disconnect = disconnect
+m.on_subscribe = subscribe
+m.on_unsubscribe = unsubscribe
+m.on_message = message
 
-print("Attempting to connect to %s" % mqtt_client.broker)
-mqtt_client.connect()
+print("Attempting to connect to %s" % m.broker)
+m.connect()
 
 print("Subscribing to %s" % mqtt_topic)
-mqtt_client.subscribe(mqtt_topic)
-
-# print("Publishing to %s" % mqtt_topic)
-# mqtt_client.publish(mqtt_topic, "Hello Broker!")
-
-# print("Unsubscribing from %s" % mqtt_topic)
-# mqtt_client.unsubscribe(mqtt_topic)
-
-# print("Disconnecting from %s" % mqtt_client.broker)
-# mqtt_client.disconnect()
+m.subscribe(mqtt_topic)
 
 while True:
-    # Poll the message queue
-    mqtt_client.loop()
-
-    # Send a new message
-    # print("Sent!")
-    # photocell_val += 1
-    time.sleep(0.25)
+    try:
+        m.is_connected()
+        m.loop()
+    except (MQTT.MMQTTException, RuntimeError):
+        network.connect()
+        m.reconnect()
